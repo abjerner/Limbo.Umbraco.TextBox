4140a6b40b7ac062882d481537043d94e0b5c551
Anders Bjerner <abjerner@skybrud.dk>
2023-07-20 16:38:33 +0200

    Updated package manifest filter to set "PackageId" property in Umbraco 12

--- src/Limbo.Umbraco.TextBox/TextBoxManifestFilter.cs
+++ src/Limbo.Umbraco.TextBox/TextBoxManifestFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
 using Umbraco.Cms.Core.Manifest;
 
 namespace Limbo.Umbraco.TextBox {
@@ -8,7 +9,9 @@ namespace Limbo.Umbraco.TextBox {
 
         /// <inheritdoc />
         public void Filter(List<PackageManifest> manifests) {
-            manifests.Add(new PackageManifest {
+
+            // Initialize a new manifest filter for this package
+            PackageManifest manifest = new() {
                 AllowPackageTelemetry = true,
                 PackageName = TextBoxPackage.Name,
                 Version = TextBoxPackage.InformationalVersion,
@@ -19,7 +22,21 @@ namespace Limbo.Umbraco.TextBox {
                 Stylesheets = new[] {
                     $"/App_Plugins/{TextBoxPackage.Alias}/Styles/Default.css"
                 }
-            });
+            };
+
+            // The "PackageId" property isn't available prior to Umbraco 12, and since the package is build against
+            // Umbraco 10, we need to use reflection for setting the property value for Umbraco 12+. Ideally this
+            // shouldn't fail, but we might at least add a try/catch to be sure
+            try {
+                PropertyInfo? property = manifest.GetType().GetProperty("PackageId");
+                property?.SetValue(manifest, TextBoxPackage.Alias);
+            } catch {
+                // We don't really care about the exception
+            }
+
+            // Append the manifest
+            manifests.Add(manifest);
+
         }
 
     }
