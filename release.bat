53e658fc7c175a4de04f47eedb3e481b720554df
Anders Bjerner <abj@limbo.works>
2026-07-06 13:46:44 +0200

    First alpha release for Umbraco 17 :tada:

--- release.bat
+++ release.bat
@@ -1,2 +1,3 @@
 @echo off
+dotnet msbuild src/Limbo.Umbraco.TextBox.Client -t:RunBuild
 dotnet build src/Limbo.Umbraco.TextBox --configuration Release /t:rebuild /t:pack -p:PackageOutputPath=../../releases/nuget
